0e6d0b56588d4b1e343885d71763a3cc7def2e28
+Sam Thongo <+samuelthongo73@gmail.com>
2023-06-13 12:28:00 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser = 'Great Sam'
+const welcomeMessage = 'Welcome to Flatbook, ${Great Sam}!'
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, ${GREAT SAM}!"
+const shortGreeting = "Welcome, ${G sam}!"
